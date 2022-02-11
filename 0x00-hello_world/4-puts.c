04cbe286c0965d19d8d06867aa534a8c701c7c4a
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-11 00:30:15 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+
+0;136;0c#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");

c7e90161d5577cb4c7a669d081ff59a8fe081f7a
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-12 15:19:45 +0000

    alfabeto

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-  char ch;
+char ch;
 
-  for (ch = 'a'; ch <= 'z'; ch++)
-  putchar(ch);
-  putchar('\n');
-  return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
 }

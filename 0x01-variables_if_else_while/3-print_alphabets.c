f0aecb5908b3af12538833ac2e67429f25eeda73
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-12 15:41:40 +0000

    ALFABETO

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Alphabet in lowercase, and then in uppercase
+ *
+ *Return: (0)
+ */
+
+int main(void)
+
+char a;
+char AB;
+for (ab = 'a' ; ab <= 'z' ; ab++)
+{
+putchar(ab);
+}
+for (AB = 'A' ; AB <= 'Z' ; AB++)
+{
+putchar(AB);
+}
+putchar ('\n');
+return (0);

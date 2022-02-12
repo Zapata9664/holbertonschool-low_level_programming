55b39303ccf070db61b705f28579bd684910ee5b
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-12 17:16:43 +0000

    add_7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
-lkasdkldasfads
+#include<stdio.h>
+
+/**
+ *Write a program that prints the lowercase alphabet in reverse
+ *
+ *Return: (0)
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}

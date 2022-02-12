8cbc0fb0fa39cd36c9f142a00b6c4f8e662eacea
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-12 16:24:32 +0000

    ls

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ *main - Write a program that prints all single digit numbers of base 10 starting from zero
+ *
+ *Return: (0)
+ */
+
+int main(void)
+{
+int i = 0;
+while(i<10)
+{
+printf("%d \n", i);
+i++
+}
+return (0)
+}

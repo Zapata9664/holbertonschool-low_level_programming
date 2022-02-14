a89cc13178937b5573d8d9c086a93804f047cefe
Zapata9664 <valentinazapatajaramill@gmail.com>
2022-02-14 17:28:47 -0500

    proyecto

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return always 0.
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 97 ; ch >= 122; ch++)
+{
+_putchar(ch);
+}
+_putchar(10);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef main
+#define main
+
+void print_alphabet(void);
+void _putchar(char);
+void print_alphabet_x10(void);
+#endif

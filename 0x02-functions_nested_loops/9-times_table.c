7cec8c2045712e7a6ec8c54fbae1a62edf48eba3
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-15 19:48:41 -0500

    agregando punto 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * main
+ *
+ * Return
+*/
+
+void times_table(void)
+{
+	int a,b,c, modul, dec;
+	for (a = 0 ; a <= 9; a++)
+	{
+		for (b = 0 ; b <= 9; b++)
+		{	
+
+			c = a * b;
+		if (b != 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+		if (c < 10 && b != 0)
+		{
+			_putchar(' ');
+			_putchar('0' + c);
+
+		}
+		else if (c >= 10)
+		{
+			modul = c % 10;
+
+			dec = (c - modul) / 10;
+
+			_putchar('0' + dec);
+			_putchar('0'+ modul);
+		}
+		else 
+		{
+			_putchar('0');
+		}
+		}
+		_putchar(10);
+	}
+
+}

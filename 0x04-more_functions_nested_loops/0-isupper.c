619f0fe83a12d9cc1738342781f0069e89b28cc8
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-17 14:54:26 -0500

    agregando 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * _isupper - print alphabet
+ * @c: letter
+ * 
+ * Return: letter
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

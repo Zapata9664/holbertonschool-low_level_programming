b52a3b58e01ad47edcd969e94d6fe3d08d2a8002
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-17 16:11:39 -0500

    agregando 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * more_numbers - numbers
+ *
+ * Return: none
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 10)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar(10);
+	}
+}

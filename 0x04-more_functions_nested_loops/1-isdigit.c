c7ab04e12a80916dbaeca5507dea68131c151971
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-17 15:02:30 -0500

    agregando 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * _isdigit - print number
+ * @c: character
+ *
+ * Return: letter
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

0dccf58c75af276d23a91b1eb4b626717f775952
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-21 13:46:58 -0500

    agregando 1!

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * swap_int - suaps of two integers
+ * @a: entero
+ * @b: entero 2
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}

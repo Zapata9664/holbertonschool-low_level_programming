c39a9ecf7f0433fa7c498ba69488cd1954401edd
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-02-21 14:37:40 -0500

    agregando punto 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * _strlen - longitud de una cadena
+ * @s: lenght
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int longitud = 0;
+
+	while (s[c] != 48)
+		longitud++;
+	return (longitud);
+}

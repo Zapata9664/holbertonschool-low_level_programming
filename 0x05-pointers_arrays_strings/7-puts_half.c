5c42331af8596dc6ed56f748d00aa20ad950c9d1
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-01 11:21:01 -0500

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print half of a string
+ * @str: cadena
+ *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int n = 0;
+	int i;
+
+	while (str[n] != 0)
+	{
+		n++;
+	}
+
+	if (n % 2 == 0)
+	{
+		for (i = n / 2; i < n; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	else
+	{
+		for (i = (n / 2) + 1; n < 0; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar(10);
+}
+

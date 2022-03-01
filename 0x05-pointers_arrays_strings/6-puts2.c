929fbfdd851cbff7c3cfbf115f0da48deaeb025d
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-01 08:11:43 -0500

    rev

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ * @str: string
+ *
+ */
+
+void puts2(char *str)
+{
+	int n = 0;
+	int i;
+
+	while (str[n] != 0)
+	{
+		n++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar(10);
+}

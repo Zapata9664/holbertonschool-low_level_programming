e9d23c01895ce3b5d4c14d121fbaa11db5563eac
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-01 13:19:00 -0500

    cpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - copies string
+ * @dest: cadena destino
+ * @src: pointer
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+
+}

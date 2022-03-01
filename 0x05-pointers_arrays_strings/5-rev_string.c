9066649d0ff714ff100569005bad5a4c04a27bdb
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-01 13:58:37 -0500

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,12 +13,13 @@ void rev_string(char *s)
 	char tmp;
 
 	while (s[i++])
+	{
 		l++;
-
-	      for (i = l -1 ; i >= l / 2; i--)
-		{
-			tmp = s[i];
-			s[i] = s[l - i - 1];
-			s[l - i - 1] = tmp;
-		}
+	}
+	for (i = l - 1; i >= l / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = tmp;
+	}
 }

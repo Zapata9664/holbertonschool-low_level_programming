c70c696a7a8272d0412a75cd24e77c9ae63e4fa6
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-02 13:10:00 -0500

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print reverse
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}

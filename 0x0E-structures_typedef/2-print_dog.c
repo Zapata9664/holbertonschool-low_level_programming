84b7176750f80bfc3781a1a2ebe935308f78633f
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-14 23:56:02 -0500

    printf

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * printf_dog - prints all the data
+ * @d: a dog structure
+ *
+ * Return: nothing
+ */
+
+void printf_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil");
+	}
+}

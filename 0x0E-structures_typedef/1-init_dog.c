5feaf01f6f82b8773599ed56ad6ebf2172d12ca2
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-14 23:39:49 -0500

    inicialize_struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - llamar estrucura
+ * @name: nombre
+ * @age: edad
+ * @owner: dueño
+ * @d: point
+ * Return: struct
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+

27fa087d5d15d4636b8b19d4f127875f176dc495
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-17 11:42:21 -0500

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,14 +2,17 @@
 #include "function_pointers.h"
 
 /**
- *
- *
+ * array_iterator - change array
+ * @array: array
+ * @size: variable
+ * @action: function
  *
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t index;
+
 	if (array != NULL || action != NULL)
 	{
 		for (index = 0; index < size; index++)

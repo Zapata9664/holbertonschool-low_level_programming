404a52915b1435156c363202860f823a240d5b2a
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-17 14:36:46 -0500

    find number

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,7 +6,9 @@
  * int_index - find a number
  * @array: array
  * @cmp: compare numbers
+ * @size: tamaño
  *
+ * Return: number
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -15,12 +17,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || cmp == NULL)
 	{
-		return(-1);
+		return (-1);
 	}
 
 	for (index = 0; index < size; index++)
 	{
 		if (cmp(array[index]) != 0)
+
 			return (index);
 	}
 	return (-1);

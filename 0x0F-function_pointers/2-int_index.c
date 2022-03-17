c1622704b532e1c63a53367fdea724f7705de95a
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-17 14:33:54 -0500

    find number

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include "function_pointers.h"
+
+/**
+ * int_index - find a number
+ * @array: array
+ * @cmp: compare numbers
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return(-1);
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+	return (-1);
+}

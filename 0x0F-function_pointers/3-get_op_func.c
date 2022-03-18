d5aea6d65f2ad9b359ac3ff36a9d6c09bfc69891
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-17 20:19:57 -0500

    clear

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-
 #include "3-calc.h"
 #include "function_pointers.h"
 
@@ -13,17 +12,17 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	  {"+", op_add},
+	  {"-", op_sub},
+	  {"*", op_mul},
+	  {"/", op_div},
+	  {"%", op_mod},
+	  {NULL, NULL}
 	};
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (i < 5)
 	{
 		if (*(ops[i].op) == *s)
 		{
@@ -31,6 +30,7 @@ int (*get_op_func(char *s))(int, int)
 		}
 		i++;
 	}
-	return (NULL);
+	printf("Error\n");
+	exit(99);
 }
 

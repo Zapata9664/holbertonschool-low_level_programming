54401b1c14e648ebae58eab3b23487a1a339580f
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-03-29 23:37:07 -0500

    make list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef _LISTS_H
 #define _LISTS_H
 #include <stdio.h>
-#include<stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,8 +22,9 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 
 
 #endif

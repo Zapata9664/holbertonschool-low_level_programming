42bae46da42dd8b9498b5c0c7dd23f7c845e6e10
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-06 22:57:30 -0500

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index
+ * @key: name
+ * @size: size
+ *
+ * Return: hash 
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	if (size == 0)
+		return (0);
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
+}

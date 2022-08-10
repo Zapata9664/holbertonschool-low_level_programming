e17620409438a3fe2eb4c44dd335fada3879bb49
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-10 11:59:19 -0500

    First commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - 
+ * 
+ * 
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t n = 0;
+    if (array == NULL)
+        return (-1);
+
+    for (n = 0; n < size; n++)
+    {   
+        printf("Value checked array[%ld] = [%d]\n", n, array[n]);
+        if (array[n] == value)
+            return (n);
+    }
+    return (-1);
+}
